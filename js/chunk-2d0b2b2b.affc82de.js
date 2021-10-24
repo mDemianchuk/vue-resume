8fd9e8c1560f8bb64041a560082f4432650cb164
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-24 21:16:43 +0000

    Deploy mDemianchuk/vue-resume to github.com/mDemianchuk/vue-resume.git:gh-pages

--- js/chunk-2d0b2b2b.affc82de.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b2b2b"],{"24e8":function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("v-container",[n("v-row",{attrs:{"no-gutters":""}},[n("h2",[t._v(t._s(t.title))])]),t._t("default")],2)},r=[],l={props:{title:{type:String,default:""}}},o=l,u=n("2877"),s=n("6544"),i=n.n(s),c=n("a523"),p=n("0fd9"),w=Object(u["a"])(o,a,r,!1,null,null,null);e["default"]=w.exports;i()(w,{VContainer:c["a"],VRow:p["a"]})}}]);
-//# sourceMappingURL=chunk-2d0b2b2b.affc82de.js.map
